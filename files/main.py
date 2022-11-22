d6f94dca6eea689f9c5d776ea362d4ef1aca0f08
George <geods73@yahoo.com>
2022-11-21 19:24:51 -0500

    Bug Fix / Add PdfReport generate functionality and bill calculation to Flatmate pays method.

--- files/main.py
+++ files/main.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 from fpdf import FPDF
 
 
@@ -5,8 +7,7 @@ class Bill:
     """
     Object that contains data about a bill, such as total amount and period of the bill.
     """
-
-    def __int__(self, amount, period):
+    def __init__(self, amount, period):
         self.amount = amount
         self.period = period
 
@@ -15,36 +16,50 @@ class Flatmate:
     """
     Creates a flatmate person who lives in the flat and pays a share of the bill.
     """
-
     def __init__(self, name, days_in_house):
         self.days_in_house = days_in_house
         self.name = name
 
-    def pays(self, bill):
-        pass
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+        return to_pay
 
 
 class PdfReport:
     """
     Creates a Pdf file that contains data about the flatmates such as their names, their due amount and the period of the bill.
     """
-
     def __init__(self, filename):
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
+
+        pdf.image("house.png", w=30, h=30)
 
         # Insert title
         pdf.set_font(family='Times', size=24, style='B')
         pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
 
         # Insert Period label and value
+        pdf.set_font(family="Times", size=14, style='B')
         pdf.cell(w=100, h=40, txt="Period:", border=1)
         pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
 
+        pdf.set_font(family="Times", size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=1)
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=1, ln=1)
+
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=1)
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=1, ln=1)
+
         pdf.output(self.filename)
+        webbrowser.open(self.filename)
 
 
 the_bill = Bill(amount=120, period="April 2021")
